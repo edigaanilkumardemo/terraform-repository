9bfa51be394ca793c2598e08eb35376a4e5bd34e
anil <edigaanilkumar2000@gmail.com>
2024-06-18 18:17:55 +0530

    userdata

--- day-9-ec2-userdata/main.tf
+++ day-9-ec2-userdata/main.tf
@@ -2,5 +2,8 @@ resource "aws_instance" "local" {
     ami = "ami-0cc9838aa7ab1dce7"
     instance_type = "t2.micro"
     key_name = "my-keypair-01"
-  
+    user_data = file("userdata.sh")     # define your path and file name
+    tags = {
+      Name = "userdata"
+    }
 }
